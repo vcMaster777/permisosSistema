d137ef787c96695a6ddc04d659f631af618814e4
vcMaster777 <aerius777@hotmail.com>
2018-03-23 19:41:27 -0500

    seg version permisos 1.2

--- app/src/main/java/com/maxfarman/www/permisos/Main2Activity.java
+++ app/src/main/java/com/maxfarman/www/permisos/Main2Activity.java
@@ -1,6 +1,7 @@
 package com.maxfarman.www.permisos;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.annotation.NonNull;
@@ -10,6 +11,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
@@ -21,11 +23,21 @@ import static android.Manifest.permission.PACKAGE_USAGE_STATS;
 public class Main2Activity extends AppCompatActivity {
     RelativeLayout l;
     Snackbar esnak;
+    Button btn_ubicacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        btn_ubicacion=findViewById(R.id.button2);
+        btn_ubicacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Main2Activity.this,MapsActivity.class));
+            }
+        });
+        btn_ubicacion.setEnabled(false);
         l=findViewById(R.id.milayout);
         String mensaje="Faltan otorgar algunos permisos.";
         esnak = Snackbar.make(l,mensaje,Snackbar.LENGTH_INDEFINITE);
@@ -87,6 +99,7 @@ public class Main2Activity extends AppCompatActivity {
     }
 
     private void iniciarApp() {
+        btn_ubicacion.setEnabled(true);
         Context c=getApplication();
         String m="La aplicación ya tiene todos los permisos necsesarios para iniciar";
         Toast.makeText(c,m, Toast.LENGTH_LONG).show();
---
+++ app/src/main/java/com/maxfarman/www/permisos/MapsActivity.java
@@ -0,0 +1,108 @@
+package com.maxfarman.www.permisos;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.OnConnectionFailedListener,
+        GoogleApiClient.ConnectionCallbacks {
+    double latitud = 0.0;
+    double longitud = 0.0;
+    private GoogleMap mMap;
+    GoogleApiClient cliente;
+    TextView t_lat, t_lon;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        t_lon = findViewById(R.id.longitud);
+        t_lat = findViewById(R.id.latitud);
+
+        cliente = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng Lima = new LatLng(latitud, longitud);
+        mMap.addMarker(new MarkerOptions().position(Lima).title("Aqui estoy"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Lima));
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location miscoordenadas = LocationServices.FusedLocationApi.getLastLocation(cliente);
+        if(miscoordenadas !=null)
+        {
+            latitud=miscoordenadas.getLatitude();
+            longitud=miscoordenadas.getLongitude();
+        }
+        else
+        {
+            latitud=0.0;
+            longitud=0.0;
+        }
+        t_lat.setText(String.valueOf(latitud));
+        t_lon.setText(String.valueOf(longitud));
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+}
